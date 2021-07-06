c8e76488d8ae89d214a2364ca2b3a9f21fdb7e3c
Caleb <belac335@gmail.com>
2021-07-07 00:59:07 +0100

    update netlify build error

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   target: "experimental-serverless-trace",
-  // images: {
-  //   domains: ["www.themealdb.com"],
-  // },
-};
-
-module.exports = {
   images: {
     domains: ["www.themealdb.com"],
   },
 };
+
+// module.exports = {
+//   images: {
+//     domains: ["www.themealdb.com"],
+//   },
+// };
