84e5eb2455d42d00e201828a63a7dd93b76f4880
Caleb <belac335@gmail.com>
2021-07-07 00:54:33 +0100

    fix: refactor next config file for the iamges in prod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   target: "experimental-serverless-trace",
+  // images: {
+  //   domains: ["www.themealdb.com"],
+  // },
+};
+
+module.exports = {
   images: {
     domains: ["www.themealdb.com"],
   },
